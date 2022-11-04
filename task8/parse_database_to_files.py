bccfafaa53c7fa832436001c1850137afb8da7a7
Arteem Bakhar <artem.bahar@innowise-group.com>
2022-11-04 12:07:00 +0300

    fixed

--- task8/parse_database_to_files.py
+++ task8/parse_database_to_files.py
@@ -11,18 +11,19 @@ def parse_database(path):
     for chunk in pd.read_csv(path, chunksize=chunksize):
         chunk['departure'] = pd.to_datetime(chunk['departure'])
         chunk['year'] = pd.DatetimeIndex(chunk['departure']).year
-        chunk['month'] = pd.DatetimeIndex(chunk['departure']).year
+        chunk['month'] = pd.DatetimeIndex(chunk['departure']).month
         for year_ in years:
             for month_ in months:
                 df_to_csv = chunk.where((chunk['year'] == year_) & (chunk['month'] == month_))
-                df_to_csv = df_to_csv[df_to_csv.notnull()]
-                del df_to_csv['year']
-                del df_to_csv['month']
-                csv_path = 'result/' + str(month_) + '-' + str(year_) + '.csv'
-                if os.path.exists(csv_path):
-                    df_to_csv.to_csv(csv_path, mode='a', index=False, header=False)
-                else:
-                    df_to_csv.to_csv(csv_path, mode='w', index=False, header=True)
+                df_to_csv = df_to_csv[(df_to_csv['year'].notnull()) & (df_to_csv['month'].notnull())]
+                if not df_to_csv.empty:
+                    del df_to_csv['year']
+                    del df_to_csv['month']
+                    csv_path = 'result/' + str(month_) + '-' + str(year_) + '.csv'
+                    if os.path.exists(csv_path):
+                        df_to_csv.to_csv(csv_path, mode='a', index=False, header=False)
+                    else:
+                        df_to_csv.to_csv(csv_path, mode='w', index=False, header=True)
 
 
 if __name__ == '__main__':
